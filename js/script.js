bdbcc3787e247aafe38c154eef41ff2aa6c93c23
yupes21 <rafli.r212101@gmail.com>
2024-09-04 01:53:46 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+function openLink(url) {
+  window.open(url, "_blank");
+}
+//pdf
+document.querySelector(".pdf-icon").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(".pdf-frame").style.display = "block";
+});
